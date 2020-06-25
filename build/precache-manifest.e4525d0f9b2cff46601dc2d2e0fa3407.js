6448f11c3412d8627d3b63abb1d0e4e83ec9ccff
paras <paraskanwar30@gmail.com>
2020-06-25 18:22:44 +0530

    added

---
+++ build/precache-manifest.e4525d0f9b2cff46601dc2d2e0fa3407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a42b59540ce8b1395ceac0ff7485263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af94dbf94f4cc1034fa7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "13d00038119b9d69395d",
+    "url": "/static/js/2.f1008f5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f1008f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af94dbf94f4cc1034fa7",
+    "url": "/static/js/main.8e24896f.chunk.js"
+  },
+  {
+    "revision": "035da2da05a009ca1d9f",
+    "url": "/static/js/runtime-main.2613f341.js"
+  }
+]);
